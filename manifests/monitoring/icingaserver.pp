e6ccb47a6685fb72098351b51f3dca4c5f6ef239
birger <131198@hig.no>
2015-10-15 14:06:33 +0200

    fjernet litt mer av koden

--- manifests/monitoring/icingaserver.pp
+++ manifests/monitoring/icingaserver.pp
@@ -38,19 +38,6 @@ class profile::monitoring::icingaserver {
   package { 'heirloom-mailx':
     ensure => latest,
   }
-#
-# manual edits: replace last line of
-# scripts/mail-service-notification.sh
-# /usr/bin/printf "%b" "$template" | mail -s "$NOTIFICATIONTYPE - $HOSTDISPLAYNAME - $SERVICEDISPLAYNAME is $SERVICESTATE" -S smtp=smtp://smtp.hig.no -S from="icinga2-monitor@skyhigh.hig.no" $USEREMAIL
-# scripts/mail-host-notification.sh
-# /usr/bin/printf "%b" "$template" | mail -s "$NOTIFICATIONTYPE - $HOSTDISPLAYNAME is $HOSTSTATE" -S smtp=smtp://smtp.hig.no -S from="icinga2-monitor@skyhigh.hig.no" $USEREMAIL
-#
-# should create icinga2::object::user and usergroup, have edited email in conf.d/users.conf for now
-# when creating these, again need to solve the problem with removing the default installed icingaadmin
-# same as linux-servers vs linux_servers
-#
-# have manually removed Warning from state in service notification /etc/icinga2/conf.d/templates.conf
-#
   icinga2::object::idomysqlconnection { 'mysql_connection':
     target_dir       => '/etc/icinga2/features-enabled',
     target_file_name => 'ido-mysql.conf',
@@ -95,41 +82,12 @@ class profile::monitoring::icingaserver {
     ignore_where   => 'host.name == "localhost"',
     target_dir     => '/etc/icinga2/objects/applys'
   }
-  icinga2::object::apply_service_to_host { 'check_hpacucli':
-    display_name   => 'hpacucli from nrpe',
-    check_command  => 'nrpe',
-    vars           => {
-                        nrpe_command => 'check_hpacucli',
-                      },
-    assign_where   => '"linux_servers" in host.groups',
-    ignore_where   => 'regex("(localhost|compute0(4|5))", host.name)',
-    target_dir     => '/etc/icinga2/objects/applys'
-  }
-#  package { 'nagios-nrpe-plugin':
-#    ensure => latest,
-#  }
 
-  class {
-# should initiate the db and the webuser here, db=icingaweb2,table=icingaweb_user,{name=data,active=1,password_hash=...}
-    '::icingaweb2':
+  class {'::icingaweb2':
       admin_users         => 'data',
       ido_db_name         => 'icinga2_data',
       ido_db_pass         => $icinga_db_password,
       ido_db_user         => 'icinga2',
       manage_apache_vhost => true;
-    
-#    '::icingaweb2::mod::deployment': # Dette burde være setup modulen! snodige greier...
-#      auth_token => '1914a82d7da612be',
-#      web_root   => '/etc/icingaweb2';
   }
-# after this, have to do:
-# ln -s /usr/share/icingaweb2/modules/setup /etc/icingaweb2/enabledModules/
-# ./bin/icingacli setup token create
-# chown icingaweb2:icingaweb2 /etc/icingaweb2/setup.token
-# usermod -a -G icingaweb2 www-data
-# service apache2 restart
-# php.ini: date.timezone = Europe/Oslo
-# chmod 775 /etc/icingaweb2
-# må config'e alt manuelt fra setup gui etterpå samt for å fullføre:
-# chmod -R g+w /etc/icingaweb2/*  
 }
